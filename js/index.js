777764c4188c8680cf509e7d0b56a3f6c2fea8d6
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2024-04-23 18:52:53 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
-const sdk = new CustomerSupportSDK('qa7nM5KuItdvhdqf_lCfD');
 window.onload=function(){
+   
+const sdk = new CustomerSupportSDK('qa7nM5KuItdvhdqf_lCfD');
    // Get the modal
 var modal = document.getElementById("myModal");
 
